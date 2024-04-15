d55686fe69c4e9918cf762e733c249eded4ee11a
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-04-15 17:59:48 +0000

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef, useCallback } from 'react';
 import styled from 'styled-components';
 import { useSearchParams } from 'react-router-dom';
 import {
-  Filters,
+  SearchFilters,
   CardGrid,
   StyledCardGrid,
   fetchAdvancedSearch,
@@ -11,11 +11,11 @@ import {
 
 // Define types for genre, year, season, format, and status
 type Option = { value: string; label: string };
-type Genre = Option;
-type Year = Option;
-type Season = Option;
-type Format = Option;
-type Status = Option;
+// type Genre = Option;
+// type Year = Option;
+// type Season = Option;
+// type Format = Option;
+// type Status = Option;
 
 const Container = styled.div`
   margin-top: 1rem;
@@ -27,22 +27,69 @@ const Container = styled.div`
   }
 `;
 
-const anyOption: Option = { value: '', label: 'Any' };
-
 const Search = () => {
-  const [searchParams /* setSearchParams */] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortParam = searchParams.get('sort');
+  // Directly initialize state from URL parameters
   const initialQuery = searchParams.get('query') || '';
-  const [query, setQuery] = useState<string>(initialQuery);
+  // Adjusting initialization to ensure non-null values
+  let initialSortDirection: 'DESC' | 'ASC' = 'DESC'; // Default to 'DESC'
+  if (sortParam) {
+    initialSortDirection = sortParam.endsWith('_DESC') ? 'DESC' : 'ASC';
+  }
+  const initialSortValue = sortParam
+    ? sortParam.replace(/(_DESC|_ASC)$/, '')
+    : 'POPULARITY_DESC';
+
+  const initialSort = {
+    value: initialSortValue,
+    label:
+      initialSortValue.replace('_DESC', '').charAt(0) +
+      initialSortValue.replace('_DESC', '').slice(1).toLowerCase(),
+  };
+  const genresParam = searchParams.get('genres');
+  const initialGenres = genresParam
+    ? genresParam.split(',').map((value) => ({ value, label: value }))
+    : [];
+
+  const initialYear = {
+    value: searchParams.get('year') || '',
+    label: searchParams.get('year') || 'Any',
+  };
+
+  const initialSeason = {
+    value: searchParams.get('season') || '',
+    label: searchParams.get('season') || 'Any',
+  };
+
+  const initialFormat = {
+    value: searchParams.get('format') || '',
+    label: searchParams.get('format') || 'Any',
+  };
+
+  const initialStatus = {
+    value: searchParams.get('status') || '',
+    label: searchParams.get('status') || 'Any',
+  };
+
+  // State hooks
+  const [query, setQuery] = useState(initialQuery);
+  const [selectedGenres, setSelectedGenres] = useState(initialGenres);
+  const [selectedYear, setSelectedYear] = useState(initialYear);
+  const [selectedSeason, setSelectedSeason] = useState(initialSeason);
+  const [selectedFormat, setSelectedFormat] = useState(initialFormat);
+  const [selectedStatus, setSelectedStatus] = useState(initialStatus);
+  const [selectedSort, setSelectedSort] = useState(initialSort);
+  const [sortDirection, setSortDirection] = useState<'DESC' | 'ASC'>(
+    initialSortDirection,
+  );
+
+  //Other logic
   const [animeData, setAnimeData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [hasNextPage, setHasNextPage] = useState<boolean>(false);
   const [page, setPage] = useState<number>(1);
   const delayTimeout = useRef<number | null>(null);
-  const [selectedGenres, setSelectedGenres] = useState<Genre[]>([]);
-  const [selectedYear, setSelectedYear] = useState<Year>(anyOption);
-  const [selectedSeason, setSelectedSeason] = useState<Season[]>([]);
-  const [selectedFormat, setSelectedFormat] = useState<Format>(anyOption);
-  const [selectedStatus, setSelectedStatus] = useState<Status>(anyOption);
 
   useEffect(() => {
     const previousTitle = document.title;
@@ -51,6 +98,24 @@ const Search = () => {
       document.title = previousTitle;
     };
   }, [query]);
+
+  const updateSearchParams = () => {
+    const params = new URLSearchParams();
+    params.set('query', query);
+    if (selectedGenres.length > 0) {
+      params.set('genres', selectedGenres.map((g) => g.value).join(','));
+    }
+    if (selectedYear.value) params.set('year', selectedYear.value);
+    if (selectedSeason.value) params.set('season', selectedSeason.value);
+    if (selectedFormat.value) params.set('format', selectedFormat.value);
+    if (selectedStatus.value) params.set('status', selectedStatus.value);
+    const sortBase = selectedSort.value.replace(/(_DESC|_ASC)$/, '');
+    const sortParam =
+      sortDirection === 'DESC' ? `${sortBase}_DESC` : `${sortBase}_ASC`;
+    params.set('sort', sortParam);
+
+    setSearchParams(params, { replace: true });
+  };
 
   useEffect(() => {
     setPage(1);
@@ -66,24 +131,22 @@ const Search = () => {
 
   const initiateFetchAdvancedSearch = useCallback(async () => {
     setIsLoading(true);
+    const sortBase = selectedSort.value.replace('_DESC', '');
+    const sortParam = sortDirection === 'DESC' ? `${sortBase}_DESC` : sortBase;
     try {
-      const yearFilter = selectedYear.value || undefined;
-      const formatFilter = selectedFormat.value || undefined;
-      const statusFilter = selectedStatus.value || undefined;
-
       const fetchedData = await fetchAdvancedSearch(query, page, 17, {
         genres: selectedGenres.map((g) => g.value),
-        year: yearFilter,
-        season: selectedSeason.map((s) => s.value).join(','),
-        format: formatFilter,
-        status: statusFilter,
+        year: selectedYear.value,
+        season: selectedSeason.value,
+        format: selectedFormat.value,
+        status: selectedStatus.value,
+        sort: [sortParam], // Ensure this is correctly formatted
       });
-
-      if (page === 1) {
-        setAnimeData(fetchedData.results);
-      } else {
-        setAnimeData((prevData) => [...prevData, ...fetchedData.results]);
-      }
+      setAnimeData(
+        page === 1
+          ? fetchedData.results
+          : [...animeData, ...fetchedData.results],
+      );
       setHasNextPage(fetchedData.hasNextPage);
     } catch (err) {
       console.error('Error fetching data:', err);
@@ -98,6 +161,8 @@ const Search = () => {
     selectedSeason,
     selectedFormat,
     selectedStatus,
+    selectedSort,
+    sortDirection,
   ]);
 
   const handleLoadMore = () => {
@@ -131,7 +196,7 @@ const Search = () => {
 
   return (
     <Container>
-      <Filters
+      <SearchFilters
         query={query}
         setQuery={setQuery}
         selectedGenres={selectedGenres}
@@ -144,8 +209,12 @@ const Search = () => {
         setSelectedFormat={setSelectedFormat}
         selectedStatus={selectedStatus}
         setSelectedStatus={setSelectedStatus}
+        selectedSort={selectedSort}
+        setSelectedSort={setSelectedSort}
+        sortDirection={sortDirection}
+        setSortDirection={setSortDirection}
+        updateSearchParams={updateSearchParams}
       />
-
       {isLoading && page === 1 ? (
         <StyledCardGrid>
           {Array.from({ length: 17 }).map((_, index) => (
@@ -158,6 +227,20 @@ const Search = () => {
           hasNextPage={hasNextPage}
           onLoadMore={handleLoadMore}
         />
+      )}
+      {!isLoading && animeData.length === 0 && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '10vh',
+            fontWeight: 'bold',
+            fontSize: '1.5rem',
+          }}
+        >
+          No Results
+        </div>
       )}
     </Container>
   );
